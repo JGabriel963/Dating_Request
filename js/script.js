6c08e0ed2b5def860a60ea95e927d531ac8826b4
João Gabriel <joaogabriel9633@gmail.com>
2023-03-09 01:39:44 -0300

    Finalizando geral of project

---
+++ js/script.js
@@ -0,0 +1,25 @@
+const wapper = document.querySelector('.wapper')
+const question = document.querySelector('.question')
+const yesBtn = document.querySelector('.yesBtn')
+const noBtn = document.querySelector('.noBtn')
+
+const noBtnRect = noBtn.getBoundingClientRect()
+const wapperRect = wapper.getBoundingClientRect()
+
+yesBtn.addEventListener('click', function() {
+    const imgCore = document.querySelector('.img-core')
+    const shandow = document.querySelector('.shandow')
+
+    question.textContent = "I Love You :)"
+    imgCore.classList.add('animation')
+    shandow.classList.add('animation')
+})
+
+noBtn.addEventListener('mouseover', () => {
+    const i = Math.floor(Math.random() * (wapperRect.width - noBtnRect.width) + 1)
+    const j = Math.floor(Math.random() * (wapperRect.height - noBtnRect.height) + 1)
+
+    noBtn.style.top = j + "px";
+    noBtn.style.left = i + "px";
+})
+
